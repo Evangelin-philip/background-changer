025ea968f2b3bc5448660983f8d246704acc6ddf
Evangelin-philip <evangelingeorge13@gmail.com>
2023-10-28 18:30:45 +0530

    Background Changer App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
+  const [bgColor,setbgColor]=useState("aquamarine")
+  const change_bg=(color)=>{
+    setbgColor(color)
+  }
+  useEffect(()=>{
+    document.body.style.backgroundColor=bgColor
+  },[bgColor])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100vh',position:'relative'}}>
+      <h1 style={{width:'100%',textAlign: "center",position:'absolute',top:'40vh',color:'purple'}}>Background Color Changer</h1>
+      <div style={{width:'100%',textAlign: "center",position:'absolute',top:'75vh',display:'flex',justifyContent:'space-evenly',alignItems:'center'}}>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'blue',width:'100px',color:'white'}}onClick={()=>{change_bg("blue")}}>Blue</button>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'red',width:'100px',color:'white'}} onClick={()=>{change_bg("red")}}>Red</button>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'orange',width:'100px',color:'white'}} onClick={()=>{change_bg("orange")}}>Orange</button>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'violet',width:'100px',color:'white'}} onClick={()=>{change_bg("violet")}}>Violet</button>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'green',width:'100px',color:'white'}} onClick={()=>{change_bg("green")}}>Green</button>
+        <button style={{height:'35px',borderColor:'yellow',borderRadius:'35px',backgroundColor:'black',width:'100px',color:'white'}} onClick={()=>{change_bg("black")}}>Black</button>
+      </div>
     </div>
   );
 }
